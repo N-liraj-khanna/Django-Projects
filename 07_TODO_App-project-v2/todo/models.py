34000e6a2173791ad8b810e56c4d8f60c6ce3098
N Liraj Khanna <lirajkhanna2002@gmail.com>
2021-07-27 10:13:20 +0530

    New projects Committed

--- 07_TODO_App-project-v2/todo/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Todo(models.Model):
-  content = models.CharField(max_length=1000)
-  date = models.DateTimeField(auto_now_add=True)
-  
-  def __str__(self):
-    return self.content
